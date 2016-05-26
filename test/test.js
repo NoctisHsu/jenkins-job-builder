0d4460bdc01e3efff044835a905ed52f1d384b14
Noctis Hsu <noctishsu@91app.com>
2016-05-26 12:14:21 +0800

    add test file

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
 var jenkinsrequest = require('../index.js');
+var expect = require('chai').expect;
 
-jenkinsrequest.SendJenkinsRequest('http://jenkins.dev:8080','NineYi.WebStore.MobileWebMall');
+describe("Test exception", function () {
+    it("The 'toThrow' matcher is for testing if a function throws an exception", function () {
+        function SendJenkinsRequest() {
+            jenkinsrequest.SendJenkinsRequest('http://jenkins.dev:8080', 'NineYi.WebStore.MobileWebMall');
+        }
+        expect(SendJenkinsRequest).to.not.throw();
+    });
+});
